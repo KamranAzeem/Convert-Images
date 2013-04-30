5fd0d02591f8b779e579c06948790a23cda091e3
Kamran Azeem <kamranazeem@gmail.com>
2013-04-30 17:48:19 +0300

    lowercase version

--- Convert-Images.sh
+++ Convert-Images.sh
@@ -4,20 +4,29 @@
 # Created: 2013-04-30
 # Summary: Loads images from one location, converts them and stores in a different location. Pretty simple haan!
 #          This may run through cron.
+################################################################################################################
 
 ####### - START - User Configuration
+
+
 # IMAGESOURCEDIRECTORY is the location where the raw images are uploaded through FTP.
+# The images will be inside separate brand-id subdirectories such as 635, 874, etc. 
+# The images files may have strange characters in the filenames such as space, + % etc. 
+# The image may contain a mix of uppercase and lowercase characters. Need to fix that all too.
 IMAGESOURCEDIRECTORY=/tmp/UploadedImages
 
 # POSTPROCESSDIRECTORY is the location where the original image will be moved to instead of deleting it.
 POSTPROCESSDIRECTORY=/tmp/PostProcessImages
 
 # TARGETDIRECTORY is a location where the processed images will be stored, to be used by the web application. This location will hold sub directories Big, Small, Medium and Zoom. Till now I see a PIC directory inside each of the mentioned directories but I do not see a reason to do that.
+# The target directory also stores images as per brand-ids, under separate sub-directories. . 
 TARGETDIRECTORY=/tmp/httpdocs/default_images
 
 # FTPUSERNAME and FTPGROUPNAMES must be specified, so that after the script finishes running, it can update the ownerships of the converted files. If not, the generated files will be owned by the user root. The files may be unreadable, or they may not be deleted from web and FTP interfaces.
-FTPUSERNAME=generalimages
-FTPGROUPNAME=webmasters
+# FTPUSERNAME=generalimages
+FTPUSERNAME=kamran
+# FTPGROUPNAME=webmasters
+FTPGROUPNAME=kamran
 
 # CONVERT is the name of the program which is used to convert/ resize images. 
 # ImageMagick package provides the command line utility convert, which does that.
@@ -38,6 +47,11 @@ SIZEZOOM=1600
 ####### - START - Sanity Checks
 echo ""
 
+if [ "$1" == "-v" ] ; then
+  VERBOSE="-verbose"
+fi
+
+
 if [ -d ${IMAGESOURCEDIRECTORY} ] && [ -d ${POSTPROCESSDIRECTORY} ] && [ ${TARGETDIRECTORY} ] ; then
   echo "IMAGE SOURCE DIRECTORY=${IMAGESOURCEDIRECTORY}"
   echo "POST PROCESS DIRECTORY=${POSTPROCESSDIRECTORY}"
@@ -46,16 +60,6 @@ if [ -d ${IMAGESOURCEDIRECTORY} ] && [ -d ${POSTPROCESSDIRECTORY} ] && [ ${TARGE
 else
   echo "Please configure the directories correctly in the script. Stopping."
   exit 9
-fi
-echo ""
-
-if [ ! -d ${TARGETDIRECTORY}/Big ] || [ ! -d ${TARGETDIRECTORY}/Medium ] || [ ! -d ${TARGETDIRECTORY}/Small ] || [ ! -d ${TARGETDIRECTORY}/Zoom ] ; then
-  echo "At least any one of Big, Medium, Small and Zoom directories does not exist inside ${TARGETDIRECTORY}."
-  echo "I will create these for you. Take it as a favor!"
-  # mkdir -p ${TARGETDIRECTORY}/{Big,Medium,Small,Zoom}
-  # chown ${FTPUSERNAME}:${FTPGROUPNAME} $TARAGETDIRECTORY} -R
-else
-  echo "Big, Medium, Small and Zoom directories exist inside ${TARGETDIRECTORY} ; moving forward."
 fi
 echo ""
 
@@ -72,24 +76,54 @@ echo ""
 if [ -x ${CONVERT} ] ; then
   echo "Coversion program found at ${CONVERT} ; moving forward."
 else
-  echo "This is silly. A conversion program is needed. Please provide the correct path in the CONVERT variable. Stopping."
-  echo "9"
+  echo "This is silly. To do this, a conversion program is needed. Please provide the correct path in the CONVERT variable. Stopping."
+  exit 9
 fi
 echo ""
 ####### - END - Sanity Checks
 
 
 ####### - START - Program Logic
-# Generate list of files for processing
-LISTOFIMAGES=$(ls ${IMAGESOURCEDIRECTORY})
 
-for i in ${LISTOFIMAGES}; do 
-  echo "Processing Image file: ${i}"
-  convert -verbose -resize ${SIZEZOOM}x   ${IMAGESOURCEDIRECTORY}/${i} ${TARGETDIRECTORY}/Zoom/${i}
-  convert -verbose -resize ${SIZEBIG}x    ${IMAGESOURCEDIRECTORY}/${i} ${TARGETDIRECTORY}/Big/${i}
-  convert -verbose -resize ${SIZEMEDIUM}x ${IMAGESOURCEDIRECTORY}/${i} ${TARGETDIRECTORY}/Medium/${i}
-  convert -verbose -resize ${SIZESMALL}x  ${IMAGESOURCEDIRECTORY}/${i} ${TARGETDIRECTORY}/Small/${i}
-  echo "--------------------------------------------------------------------------------------------------------------"
+# List of Directories:
+# ls -l  --time-style=long-iso /tmp/UploadedImages/  | grep ^d | cut -d " " -f8
+BRANDLIST=$(ls -l  --time-style=long-iso ${IMAGESOURCEDIRECTORY} | grep ^d | cut -d " " -f8)
+
+for BRAND in ${BRANDLIST}; do
+ 
+
+
+  if [ ! -d ${TARGETDIRECTORY}/Big/${BRAND}/ ] || [ ! -d ${TARGETDIRECTORY}/Medium/${BRAND}/ ] || [ ! -d ${TARGETDIRECTORY}/Small/${BRAND}/ ] || [ ! -d ${TARGETDIRECTORY}/Zoom/${BRAND}/ ] ; then
+    echo "The brand-id directory ${BRAND} does not exist inside ${TARGETDIRECTORY}/{Big,Medium,Small,Zoom}"
+    echo "I will create this for you, as will as the PIC directory inside each brand-id directory. Take it as a favor!"
+    echo "Executing: mkdir -p ${TARGETDIRECTORY}/{Big,Medium,Small,Zoom}/${BRAND}/PIC"
+    mkdir -p ${TARGETDIRECTORY}/{Big,Medium,Small,Zoom}/${BRAND}/PIC
+  else
+    echo "The brand-id directory ${BRAND} already exist inside ${TARGETDIRECTORY}/{Big,Medium,Small,Zoom}; moving forward."
+  fi
+  echo ""
+
+
+  # First rename the image files in the source directory by converting its name from uppercase to lowercase.
+  for FILE in ${IMAGESOURCEDIRECTORY}/${BRAND}/* ;
+    LOWERCASENAME="$( echo ${FILE} | tr '[:upper:]' '[:lower:]' )" 
+    do mv ${IMAGESOURCEDIRECTORY}/${BRAND}/"${FILE}" ${IMAGESOURCEDIRECTORY}/${BRAND}/"${LOWERCASENAME}"
+  done
+
+
+  # Generate list of files for processing in this directory
+  LISTOFIMAGES=$(ls ${IMAGESOURCEDIRECTORY}/${BRAND}/)
+  
+  for IMAGE in ${LISTOFIMAGES}; do 
+    echo "Processing Image file: ${IMAGE}"
+    echo "---------------------"
+    convert ${VERBOSE} -resize ${SIZEZOOM}x   ${IMAGESOURCEDIRECTORY}/${BRAND}/${IMAGE} ${TARGETDIRECTORY}/Zoom/${BRAND}/PIC/${IMAGE}
+    convert ${VERBOSE} -resize ${SIZEBIG}x    ${IMAGESOURCEDIRECTORY}/${BRAND}/${IMAGE} ${TARGETDIRECTORY}/Big/${BRAND}/PIC/${IMAGE}
+    convert ${VERBOSE} -resize ${SIZEMEDIUM}x ${IMAGESOURCEDIRECTORY}/${BRAND}/${IMAGE} ${TARGETDIRECTORY}/Medium/${BRAND}/PIC/${IMAGE}
+    convert ${VERBOSE} -resize ${SIZESMALL}x  ${IMAGESOURCEDIRECTORY}/${BRAND}/${IMAGE} ${TARGETDIRECTORY}/Small/${BRAND}/PIC/${IMAGE}
+  done
 done
+
+chown ${FTPUSERNAME}:${FTPGROUPNAME} ${TARGETDIRECTORY} -R
 
 ####### - END - Program Logic
