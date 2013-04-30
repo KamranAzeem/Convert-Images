5bb63c00a171df3e65700a22242cc9eb7ed93dae
Kamran Azeem <kamranazeem@gmail.com>
2013-04-30 13:05:57 +0300

    first working version

--- Convert-Images.sh
+++ Convert-Images.sh
@@ -12,19 +12,30 @@ IMAGESOURCEDIRECTORY=/tmp/UploadedImages
 # POSTPROCESSDIRECTORY is the location where the original image will be moved to instead of deleting it.
 POSTPROCESSDIRECTORY=/tmp/PostProcessImages
 
-# TARGETDIRECTORY is a location where the processed images will be stored, to be used by the web application. This location will hold sub directories BIG, SMALL, MEDIUM and ZOOM. Till now I see a PIC directory inside each of the mentioned directories but I do not see a reason to do that.
+# TARGETDIRECTORY is a location where the processed images will be stored, to be used by the web application. This location will hold sub directories Big, Small, Medium and Zoom. Till now I see a PIC directory inside each of the mentioned directories but I do not see a reason to do that.
 TARGETDIRECTORY=/tmp/httpdocs/default_images
 
 # FTPUSERNAME and FTPGROUPNAMES must be specified, so that after the script finishes running, it can update the ownerships of the converted files. If not, the generated files will be owned by the user root. The files may be unreadable, or they may not be deleted from web and FTP interfaces.
 FTPUSERNAME=generalimages
 FTPGROUPNAME=webmasters
 
+# CONVERT is the name of the program which is used to convert/ resize images. 
+# ImageMagick package provides the command line utility convert, which does that.
+CONVERT="/usr/bin/convert"
+
+# Sizes to convert the source image into target images.
+SIZEBIG=600
+SIZEMEDIUM=190
+SIZESMALL=50
+SIZEZOOM=1600
+
+
 ####### - END - User Configuration
 
 # There is nothing for the user to modify below.
 
 
-###### - START - Program Logic
+####### - START - Sanity Checks
 echo ""
 
 if [ -d ${IMAGESOURCEDIRECTORY} ] && [ -d ${POSTPROCESSDIRECTORY} ] && [ ${TARGETDIRECTORY} ] ; then
@@ -36,7 +47,16 @@ else
   echo "Please configure the directories correctly in the script. Stopping."
   exit 9
 fi
+echo ""
 
+if [ ! -d ${TARGETDIRECTORY}/Big ] || [ ! -d ${TARGETDIRECTORY}/Medium ] || [ ! -d ${TARGETDIRECTORY}/Small ] || [ ! -d ${TARGETDIRECTORY}/Zoom ] ; then
+  echo "At least any one of Big, Medium, Small and Zoom directories does not exist inside ${TARGETDIRECTORY}."
+  echo "I will create these for you. Take it as a favor!"
+  # mkdir -p ${TARGETDIRECTORY}/{Big,Medium,Small,Zoom}
+  # chown ${FTPUSERNAME}:${FTPGROUPNAME} $TARAGETDIRECTORY} -R
+else
+  echo "Big, Medium, Small and Zoom directories exist inside ${TARGETDIRECTORY} ; moving forward."
+fi
 echo ""
 
 if  [ "${FTPUSERNAME}" == "" ] ||  [ "${FTPGROUPNAME}" == "" ] ; then
@@ -49,5 +69,27 @@ else
 fi  
 echo ""
 
-###### - END - Program Logic
+if [ -x ${CONVERT} ] ; then
+  echo "Coversion program found at ${CONVERT} ; moving forward."
+else
+  echo "This is silly. A conversion program is needed. Please provide the correct path in the CONVERT variable. Stopping."
+  echo "9"
+fi
+echo ""
+####### - END - Sanity Checks
 
+
+####### - START - Program Logic
+# Generate list of files for processing
+LISTOFIMAGES=$(ls ${IMAGESOURCEDIRECTORY})
+
+for i in ${LISTOFIMAGES}; do 
+  echo "Processing Image file: ${i}"
+  convert -verbose -resize ${SIZEZOOM}x   ${IMAGESOURCEDIRECTORY}/${i} ${TARGETDIRECTORY}/Zoom/${i}
+  convert -verbose -resize ${SIZEBIG}x    ${IMAGESOURCEDIRECTORY}/${i} ${TARGETDIRECTORY}/Big/${i}
+  convert -verbose -resize ${SIZEMEDIUM}x ${IMAGESOURCEDIRECTORY}/${i} ${TARGETDIRECTORY}/Medium/${i}
+  convert -verbose -resize ${SIZESMALL}x  ${IMAGESOURCEDIRECTORY}/${i} ${TARGETDIRECTORY}/Small/${i}
+  echo "--------------------------------------------------------------------------------------------------------------"
+done
+
+####### - END - Program Logic
